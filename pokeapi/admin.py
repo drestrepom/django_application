5a40506f478fd91b800761c7a2b7ce6e6541b873
Diego Restrepo <restrepomesadiego@gmail.com>
2021-10-12 23:13:24 -0500

    feat(back): add models to admin

--- pokeapi/admin.py
+++ pokeapi/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import (
     admin,
 )
+from pokeapi.models import (
+    Evolution,
+    Pokemon,
+    Specie,
+)
 
 # Register your models here.
+admin.site.register(Specie)
+admin.site.register(Pokemon)
+admin.site.register(Evolution)
